c4125f0917a028165ea15d5a310b4815e05d0512
iPoolito <poolortegas@gmail.com>
2022-12-12 18:15:45 -0600

    Setting up handlebars,socket.io, routes and views

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import __dirname from './utils.js'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+import viewsRouter from './routes/views.router.js'
+
+const app = express()
+const httpServer = app.listen(8080, () => console.log("Listening in PORT 8080"))
+
+//Crear un servidor para sockets 
+const socketServer = new Server(httpServer)
+
+//configuramos moto de plantillas
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+//Importante para archivos de js y css en plantillas
+app.use(express.static(__dirname + '/public'))
+app.use('/', viewsRouter)
